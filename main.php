a65d3966634659d4cd2f7a7f58e81360a4b7d9e1
matsyk95 <matsyk998@gmail.com>
2017-01-10 18:20:19 +0100

    Create main.php

---
+++ main.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=utf-8">
+        <META NAME="Author" CONTENT="Mateusz Sykut">
+        <link rel="stylesheet" href="style_projekt4.css" type="text/css" media="all" />	
+        <script type="text/javascript" src="style/js.js" charset="UTF-8"></script>
+        <title>Projekt 4</title>
+    </head>
+    <body>
+	        ﻿<?php
+?>
+	            <div id="content">
+
+                <h1>Projekt 4</h1>
+	                <a href="#" id="upload1" onClick="zmien(this.id)">Upload</a>
+                <a href="#" id="upload2" onClick="zmien(this.id)">Upload2</a>
+                <a href="#" id="show" onClick="zmien(this.id)">show</a>
+				 <div id="nadramka">
+                    <iframe src="show.php"  id="ramka">
+                    </iframe>
+					</div>
+				</div>
+	</body>
+	</html>
